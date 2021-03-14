2119933589b5362ce0c1e1b85cfbca90e2459214
Dark-Gran <74474188+Dark-Gran@users.noreply.github.com>
2021-03-14 17:16:24 +0100

    Add files via upload

---
+++ payment.php
@@ -0,0 +1,142 @@
+﻿<!DOCTYPE html>
+<!--Copyright 2020 Filip Randák - DarkGran-->
+<html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8" />
+<link rel="canonical" href="https://www.farstar.tech" />
+<link rel="stylesheet" type="text/css" href="styling.css">
+<title>FARSTAR: Exodus</title>
+</head>
+
+<body>
+<div align="center"><img src="fslogo.png" alt="FARSTAR: Exodus"></div>
+
+<?php
+$err = false;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+ 	$data = $_POST;
+}
+if (isset($data) && isset($data['d']) && isset($data['r']) && isset($data['print']) && isset($data['pval'])) {
+	$d = $data['d'];
+	$r = $data['r'];
+	$print = $data['print'];
+	$pval = $data['pval'];
+	switch ($pval) {
+		case 0:
+			$pstr = "400 Credits";
+			break;
+		case 1:
+			$pstr = "1200 Credits";
+			break;
+		case 2:
+			$pstr = "3000 Credits";
+			break;
+		case 3:
+			$pstr = "20000 Credits";
+			break;
+	}
+	if (!isset($pstr)) {
+		$err = true;		
+	}
+} else {
+	$err = true;
+}
+?>
+
+<div class="container">
+
+<br><br>
+
+<?php if (!$err):?>
+
+<script src="https://www.paypal.com/sdk/js?client-id=AaLhs21QgdzTXO8h6Awj3dXXBoCSKZ1xLuOPsp8_0BZwx0689EG2tZFkLSiyo81EuvnfPfbg4-1gt3S0"></script>
+<div class="smallcontainer" id="paypal-button-container">
+<br>
+<div align="center" class="bigunderline">
+<?php
+echo 'Buying ' . $pstr  . '.';
+?>
+</div>
+<br><br>
+<script type="text/javascript">
+var ppButts;
+var pval = '<?php echo $pval ?>';
+var pp = '0.00';
+switch (pval) {
+	case "0":
+		pp = '2.00';
+		break;
+	case "1":
+		pp = '5.00';
+		break;
+	case "2":
+		pp = '10.00';
+		break;
+	case "3":
+		pp = '60.00';
+		break;
+}
+var userstr = '<?php echo $print ?>' + '&' + '<?php echo $r ?>';
+ppButts = new paypal.Buttons({
+	style: {
+		layout: 'vertical',
+		color: 'blue'
+	},
+	createOrder: function(data, actions) {
+     	return actions.order.create({
+        application_context: {
+	  		shipping_preference:'NO_SHIPPING',
+	  		brand_name:'FARSTAR: Exodus',
+	  		user_action:'PAY_NOW'
+		},
+       	purchase_units: [{
+			amount: {
+				value: pp
+			},
+			custom_id: userstr
+		}]
+     	});
+   	},
+   	onApprove: function(data, actions) {
+     	return actions.order.capture().then(function(details) {
+        window.location.replace("/success.html");
+     	});
+   	}
+});
+ppButts.render("#paypal-button-container");
+</script>
+</div>
+<br><br><br><br><br><br><br>
+
+<?php else: 
+$err = true;
+?>
+<br><br>
+<div align="center" id="Void">	
+<i>Right place at the wrong time...<br><br>
+Did we get lost in the Void?<br><br>
+Must have been my concentration...<br><br>
+...<br><br>
+Shall we try again?</i>
+</div>
+<br><br><br><br>
+
+<?php endif; ?>
+<?php if (!$err):?>
+<div id="backbutton" align="center">
+<a href="shop.php?d=<?php echo $d ?>&r=<?php echo $r ?>&print=<?php echo $print ?>"><img src="images/back.png" id="back"></a>
+</div>
+<?php endif; ?>
+<?php if ($err):?>
+<br><br><br><br><br><br>
+<?php endif; ?>
+
+<hr>
+<div class="footer" align="left"><p> &#x00a9; 2020 Filip Randák - DarkGran (ICO/CRN: 01800485)</p></div>
+<div align="center"><a href="index.html"><img alt="" src="fsgran.png"></a></div>
+
+</div>
+</body>
+</html>
---
+++ shop.php
@@ -0,0 +1,81 @@
+﻿<!DOCTYPE html>
+<!--Copyright 2020 Filip Randák - DarkGran-->
+<html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8" />
+<link rel="canonical" href="https://www.farstar.tech" />
+<link rel="stylesheet" type="text/css" href="styling.css">
+<title>FARSTAR: Exodus</title>
+</head>
+
+<body>
+<div align="center"><img src="fslogo.png" alt="FARSTAR: Exodus"></div>
+
+<div class="container">
+
+<br><br>
+
+<?php if (isset($_GET) && isset($_GET['d']) && isset($_GET['r'])  && $_GET['r'] != "0" && isset($_GET['print'])):?>
+
+<div class="smallcontainer" id="creditspage">
+<div class="chaptername" align="center">Choose a Package:</div>
+<br>
+
+<form class="unselectable" target="_self" action="/payment.php" method="POST">
+<div class="custom-select" align="left">
+<input type="radio" id="0" name="pval" value="0">
+<label class="custom-label" for="0">400 Credits - 2 USD</label><br>
+<input type="radio" id="1" name="pval" value="1" checked="checked">
+<label class="custom-label"for="1">1200 Credits - 5 USD</label><br>
+<input type="radio" id="2" name="pval" value="2">
+<label class="custom-label"for="2">3000 Credits - 10 USD</label><br>
+<input type="radio" id="3" name="pval" value="3">
+<label class="custom-label"for="3">20000 Credits - 60 USD</label>
+</div>
+
+<br>
+<?php
+$data = $_GET;
+$d = $data['d'];
+$r = $data['r'];
+$print = $data['print'];
+$userstr = "(user:" . $print . ")";
+echo htmlspecialchars($userstr, ENT_QUOTES, 'UTF-8');
+?>
+<br><br><br>
+<div align="center">
+<div align="center" class="buynow">
+<input class="unselectable" type="hidden" name="d" value="<?php echo $d ?>" />
+<input class="unselectable" type="hidden" name="r" value="<?php echo $r ?>" />
+<input class="unselectable" type="hidden" name="print" value="<?php echo $print ?>" />
+<input class="unselectable" type="image" src="images/buynow.png" border="0" alt="[buy now]" />
+</form>
+</div>
+</div>
+</div>
+
+<?php else: ?>
+<br><br>
+<div align="center" id="Void">	
+<i>Right place at the wrong time...<br><br>
+Did we get lost in the Void?<br><br>
+Must have been my concentration...<br><br>
+...<br><br>
+Shall we try again?</i>
+</div>
+<br><br><br><br>
+
+
+<?php endif; ?>
+
+
+<br><br><br><br><br><br>
+<hr>
+<div class="footer" align="left"><p> &#x00a9; 2020 Filip Randák - DarkGran (ICO/CRN: 01800485)</p></div>
+<div align="center"><a href="index.html"><img alt="" src="fsgran.png"></a></div>
+
+</div>
+</body>
+</html>
